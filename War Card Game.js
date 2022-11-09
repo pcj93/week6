6352f4902916be339ded7b321f206eb0aa5b997b
pcj93 <juniorcossio93@gmail.com>
2022-11-09 08:52:46 -0700

    updated rough draft

--- War Card Game.js
+++ War Card Game.js
@@ -1,9 +1,9 @@
 //values that I had no idea how to do so made them into arrays
 const cardSuits= ["♠", "♣", "♥", "♦"]
-const cardValues = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
 const player1Points= []
 const player2Points= []
-const playerSum = 0;
+// let playerSum = 0;
 
 //classes that i used for Cards,Deck,Player
 class Deck {
@@ -15,13 +15,13 @@ class Deck {
         return this.cards.length
     }
 
-    pop(){
-        return this.cards.shift()
-    }
+    // pop(){
+    //     return this.cards.shift()
+    // }
 
-    push(card){
-        this.cards.push(card)
-    }
+     push(card){
+         this.cards.push(card)
+     }
     
 
    shuffle() {
@@ -41,10 +41,17 @@ class Card {
     }
 }
 
+// class Player {
+//     constructor(player1, player2){
+//         this.player1 = player1
+//         this.player2 = player2
+//     }
+// }
 class Player {
-    constructor(player1, player2){
-        this.player1 = player1
-        this.player2 = player2
+    constructor(name){
+        this.name = name,
+        this.hand = [],
+        this.score = 0
     }
 }
 
@@ -56,20 +63,43 @@ function deckShuffled(){
     })
 }
 
+// function array1(points) {
+//     playerSum = array1.reduce((previousValue, currentValue) => {
+//       return previousValue + currentValue;
+//     });
+//     return playerSum;
+// }
+
 //cards are being handed out with 26 per player
-startWar()
+//startWar()
 function startWar(){
 const newShoot = new Deck()
 newShoot.shuffle()
 
 const midWar = Math.ceil(newShoot.numberOfCards /2)
-player1 = new Deck(newShoot.cards.slice(0, midWar))
-player2 = new Deck(newShoot.cards.slice(midWar, newShoot.numberOfCards))
-console.log(player1,player2)
-}
 
-let playerOneCard = this.player1.pop();
-let playerTwoCard = this.player2.pop();
-for (let i = 1; i <= 26 ; i++) {
-    console.log (playerOneCard,playerTwoCard)
-}
+let playerOne = new Player("Sam")
+let playerTwo = new Player ("Pedro")
+playerOne.hand = newShoot.cards.slice(0, midWar)
+playerTwo.hand = newShoot.cards.slice(midWar, newShoot.numberOfCards)
+console.log(playerOne, playerTwo)
+
+  let testCard1 = playerOne
+  let testCard2 = playerTwo
+ for (let i = 0; i < 26 ; i++) {
+     console.log(testCard1.hand[i].value, testCard2.hand[i].value)
+    if (testCard1.hand[i].value > testCard2.hand[i].value){
+              //console.log(testCard1.hand[i].value, testCard2.hand[i].value)
+                testCard1.score += 1
+              } else if (testCard2.hand[i].value > testCard1.hand[i].value){
+                 //console.log(testCard1,testCard2)
+                 testCard2.score += 1
+ } else{
+    testCard1.hand[i].value == testCard2.hand[i].value
+    return "Tie Draw"
+ }
+ console.log(testCard1.score, testCard2.score)
+ }
+ }
+
+startWar()
